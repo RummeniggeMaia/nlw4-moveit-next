42305c56be4017c84a0350547018785b48e2556a
Rummenigge Maia <rummeniggemaia@gmail.com>
2021-02-23 14:31:08 -0300

    Criar hook do countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,21 +1,46 @@
+import { useEffect, useState } from 'react'
 import styles from '../styles/components/Countdown.module.css'
 
 const Countdown = () => {
+  const [time, setTime] = useState(25 * 60)
+  const [active, setActive] = useState(false)
+
+  const mins = Math.floor(time / 60)
+  const secs = time % 60
+
+  const [minL, minR]= String(mins).padStart(2, '0').split('')
+  const [secL, secR]= String(secs).padStart(2, '0').split('')
+
+  const startCD = () => {
+    setActive(true)
+  }
+
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time -1)
+      }, 1000)
+    }
+  }, [active, time])
+
   return(
     <div>
       <div className={ styles.countdownContainer }>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{ minL }</span>
+          <span>{ minR }</span>
         </div>
         <span>:</span>
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{ secL }</span>
+          <span>{ secR }</span>
         </div>
       </div>
 
-      <button type="button" className={ styles.countdownButton }>
+      <button 
+        type="button" 
+        className={ styles.countdownButton }
+        onClick={ startCD }>
         Iniciar um ciclo
       </button>
     </div>
