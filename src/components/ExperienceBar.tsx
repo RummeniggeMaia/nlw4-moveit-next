1881b24713e1fce382e8896a2372dc1afc3b4b56
Rummenigge Maia <rummeniggemaia@gmail.com>
2021-02-25 11:35:20 -0300

    Adicionar currentExperience na ExperienceBar

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,15 +1,18 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css'
 
 const ExperienceBar = (props: any) => {
+  const { currentExperience } = useContext(ChallengesContext)
   return (
-    <header className={ styles.experienceBar }>
-      <span>8 xp</span>
-        <div>
-          <div style={{ width: '50%'}} />
-          <span className={ styles.currentExperience } style={{ left: '50%'}}>
-            300 xp
+    <header className={styles.experienceBar}>
+      <span> {currentExperience} </span>
+      <div>
+        <div style={{ width: '50%' }} />
+        <span className={styles.currentExperience} style={{ left: '50%' }}>
+          300 xp
           </span>
-        </div>
+      </div>
       <span>688 xp</span>
     </header>
   );
