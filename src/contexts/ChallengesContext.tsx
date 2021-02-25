de0ce8ef1add37e34ee07995910a698ddec830b8
Rummenigge Maia <rummeniggemaia@gmail.com>
2021-02-25 16:49:23 -0300

    Implementar completedChallenge

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -16,6 +16,7 @@ interface ChallengesContextData {
   levelUp: () => void
   startNewChallenge: () => void
   resetChallenge: () => void
+  completeChallenge: () => void
 }
 
 interface ChallengesProviderProps {
@@ -48,6 +49,25 @@ export const ChallengesProvider = ({ children }: ChallengesProviderProps) => {
     setActiveChallenge(null)
   }
 
+  const completeChallenge = () => {
+    if (!activeChallenge) {
+      return
+    }
+
+    const { amount } = activeChallenge
+
+    let finalExperience = currentExperience + amount
+
+    if (finalExperience >= experienceToNextLevel) {
+      finalExperience = finalExperience - experienceToNextLevel
+      levelUp()
+    }
+
+    setCurrentExperience(finalExperience)
+    setActiveChallenge(null)
+    setChallengesCompleted(challengesCompleted + 1)
+  }
+
   return (
     <ChallengesContext.Provider value={{
       level,
@@ -58,6 +78,7 @@ export const ChallengesProvider = ({ children }: ChallengesProviderProps) => {
       levelUp,
       startNewChallenge,
       resetChallenge,
+      completeChallenge,
     }}>
       { children}
     </ChallengesContext.Provider>
