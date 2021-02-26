b8dc38f763afbc4987b51b6830c4af880c9cf80e
Rummenigge Maia <rummeniggemaia@gmail.com>
2021-02-26 14:10:06 -0300

    Implementar getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,25 +9,42 @@ import styles from '../styles/pages/Home.module.css'
 import ChallengeBox from '../components/ChallengeBox'
 import { CountdownProvider } from '../contexts/CountdownContext'
 
-const Home = (props: any) => (
-  <div className={styles.container}>
-    <Head>
-      <title>Início | move.it</title>
-    </Head>
-    <ExperienceBar />
-    <CountdownProvider>
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
-    </CountdownProvider>
-  </div>
-)
+const Home = (props: any) => {
+
+  console.log(props)
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+      <ExperienceBar />
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
+    </div>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const user = {
+    level: 1,
+    currentExperience: 50,
+    challengesCompleted: 2,
+  }
+
+  return {
+    props: user
+  }
+}
 
 export default Home
