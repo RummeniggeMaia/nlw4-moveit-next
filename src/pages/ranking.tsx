e51c19f97753ee19d6c597a41fd1033a759d51f9
Rummenigge Maia <rummeniggemaia@gmail.com>
2021-02-28 20:37:00 -0300

    Implementando tabela de ranking

--- src/pages/ranking.tsx
+++ src/pages/ranking.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import styles from '../styles/pages/Ranking.module.css'
 
 import Layout from './layout'
@@ -10,11 +11,35 @@ const Ranking = () => {
       currentExperience={0}
       challengesCompleted={0}
       title="Ranking">
-      <ul>
-        <li>User 1</li>
-        <li>User 2</li>
-        <li>User 3</li>
-      </ul>
+
+      <div className={styles.container}>
+        <strong>Leaderboard</strong>
+        <div className={styles.table}>
+          <header>
+            <span>POSIÇÂO</span>
+            <span>USUÁRIO</span>
+            <span>DESAFIOS</span>
+            <span>EXPERIÊNCIA</span>
+          </header>
+          <main>
+            <div>
+              <span className={styles.columnPosition}>1</span>
+              <span className={styles.columnUser}>
+                <img src="https://github.com/RummeniggeMaia.png" alt="Rummenigge Maia" />
+                <div>
+                  <strong>Rummenigge Maia</strong>
+                  <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level 1
+                  </p>
+                </div>
+              </span>
+              <span><strong>127</strong> completados</span>
+              <span><strong>154000</strong> xp</span>
+            </div>
+          </main>
+        </div>
+      </div>
     </Layout>
   )
 }
