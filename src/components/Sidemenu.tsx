6246d49e589110997904d7b374fe3aae9719dbea
Rummenigge Maia <rummeniggemaia@gmail.com>
2021-02-27 23:09:16 -0300

    Finalizar Sidemenu

--- src/components/Sidemenu.tsx
+++ src/components/Sidemenu.tsx
@@ -1,28 +1,33 @@
-import styles from '../styles/components/Sidemenu.module.css'
-
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
+import styles from '../styles/components/Sidemenu.module.css'
+
+
 const Sidemenu = () => {
+
+  const router = useRouter()
+  const name = router.pathname
 
   return (
     <div className={styles.sidemenuContainer}>
       <img src="/icons/logo.svg" alt="Logotipo" />
       <div></div>
       <div>
-        <div className={styles.menuItem}>
-          <span></span>
-          <Link href="/">
-            <img src="/icons/home.svg" alt="Home" />
-          </Link>
-          <div></div>
-        </div>
-        <div className={styles.menuItem}>
-          <span></span>
-          <Link href="/ranking">
-            <img src="/icons/award.svg" alt="award" />
-          </Link>
-          <div></div>
-        </div>
+        <Link href="/">
+          <div className={styles.menuItem}>
+            {name === '/' ? <span></span> : <div></div>}
+            <img src={`/icons/home_${name === '/' ? 'on' : 'off'}.svg`} alt="Home" />
+            <div></div>
+          </div>
+        </Link>
+        <Link href="/ranking">
+          <div className={styles.menuItem}>
+            {name === '/ranking' ? <span></span> : <div></div>}
+            <img src={`/icons/award_${name === '/ranking' ? 'on' : 'off'}.svg`} alt="Ranking" />
+            <div></div>
+          </div>
+        </Link>
       </div>
       <div></div>
     </div>
